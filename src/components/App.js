1f35464869716c10f4c345e0e9b4d94f3073ad05
Kiyomarthi <a.kiyomarthi@gmail.com>
2024-04-30 15:43:00 +0330

    feat: add the new component called FinishScreen add highscore and finish page by modify the nextbuttton to finish on last question

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import StartScreen from "./StartScreen.js";
 import Question from "./Question.js";
 import NextButton from "./NextButton.js";
 import Progress from "./Progress.js";
+import FinishedScreen from "./FinishedScreen.js";
 
 const initialState = {
   questions: [],
@@ -14,6 +15,7 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -35,15 +37,20 @@ function reducer(state, action) {
       };
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
     default:
       throw new Error("action unkown");
   }
 }
 function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
+  const [{ questions, status, index, answer, points, highscore }, dispatch] =
+    useReducer(reducer, initialState);
   const numQuestions = questions.length;
   const maxPassiblePoints = questions.reduce(
     (sum, curr) => sum + curr.points,
@@ -79,8 +86,20 @@ function App() {
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton dispatch={dispatch} answer={answer} />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              index={index}
+              numQuestions={numQuestions}
+            />
           </>
+        )}
+        {status === "finished" && (
+          <FinishedScreen
+            points={points}
+            maxPassiblePoints={maxPassiblePoints}
+            highscore={highscore}
+          />
         )}
       </Main>
     </div>
---
+++ src/components/FinishedScreen.js
@@ -0,0 +1,22 @@
+export default function FinishedScreen({
+  points,
+  maxPassiblePoints,
+  highscore,
+}) {
+  const percentage = (points / maxPassiblePoints) * 100;
+  let emoji;
+  if (percentage === 100) emoji = "🏆";
+  if (percentage >= 80 && percentage < 100) emoji = "👏";
+  if (percentage >= 50 && percentage < 80) emoji = "😃";
+  if (percentage >= 0 && percentage < 50) emoji = "🙄";
+  if (percentage === 0) emoji = "🤦‍♂️";
+  return (
+    <>
+      <p className="result">
+        {emoji} you scord {points} out of {maxPassiblePoints} (
+        {Math.ceil(percentage)}%)
+      </p>
+      <p className="highscore">highscore: {highscore} points </p>
+    </>
+  );
+}
