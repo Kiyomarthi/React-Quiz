e05ec597c394bc5973e47e24f1e4867ffae6729f
Kiyomarthi <a.kiyomarthi@gmail.com>
2024-04-30 08:01:29 +0330

    feat: add the Main component add the Main.js file in src folder import in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import Header from "./Header.js";
+import Main from "./Main.js";
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main>
+        <p>1/15</p>
+        <p>question?</p>
+      </Main>
     </div>
   );
 }
