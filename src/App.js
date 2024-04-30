34c0914c0888f69efd0b3d688c24a38f15130322
Kiyomarthi <a.kiyomarthi@gmail.com>
2024-04-30 08:43:17 +0330

    feat: add fetch API and stat 1: use from useEffect for fetch data 2: use from useReducer for state and action for apdate data from API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header.js";
 import Main from "./Main.js";
+import { type } from "@testing-library/user-event/dist/type/index.js";
+
+const initialState = {
+  questions: [],
+  status: "Loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "Error" };
+    default:
+      throw new Error("action unkown");
+  }
+}
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="App">
       <Header />
