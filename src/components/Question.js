d1e28b605e89d8dfc571a36c3292d6b68875c244
Kiyomarthi <a.kiyomarthi@gmail.com>
2024-04-30 10:18:04 +0330

    feat(options-poins): show the correct answer after click and get the point

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,10 @@
 import Option from "./Option";
 
-export default function Question({ question }) {
+export default function Question({ question, dispatch, answer }) {
   return (
     <div>
       <h3>{question.question}</h3>
-      <Option question={question} />
+      <Option question={question} dispatch={dispatch} answer={answer} />
     </div>
   );
 }
